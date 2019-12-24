9589b1a14730604407674b5228a6cbb85712da83
Jorge Alvarado <jalvaradoas39@outlook.com>
2019-12-24 14:23:57 -0500

    Switch NPM "semantic-ui-css" script for CDN

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
 import './index.css';
-import 'semantic-ui-css/semantic.min.css'
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 
